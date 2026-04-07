6bf5bb40186318c7926cd5d2d7159430e4021dc7
Javad Pourmostafa <j.pourmostafa@tilburguniversity.edu>
2026-04-07 13:40:36 +0200

    class attributes are added so students would be able to see the difference

--- AP4CSAI-800829-B-6/Lecture5/E04_attributes.py
+++ AP4CSAI-800829-B-6/Lecture5/E04_attributes.py
@@ -17,3 +17,19 @@ p2.y = 6
 # Print the attributes of each object
 print(p1.x, p1.y)
 print(p2.x, p2.y)
+
+##########################
+# Class attributes are defined in the class body
+# They are shared by all instances of the class
+
+class Test:
+    x = 0
+    y = 10
+
+t1 = Test()
+t2 = Test()
+
+t1.x = 5  # creates an instance attribute for t1 only
+
+print(t1.x)  # 5  (instance attribute)
+print(t2.x)  # 0  (still class attribute)
