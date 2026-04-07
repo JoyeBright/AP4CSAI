db25e143e3c2cd617716175a74f94cca15451f1b
Javad Pourmostafa <j.pourmostafa@tilburguniversity.edu>
2026-04-07 13:41:59 +0200

    commenting out the class specific attributes

--- AP4CSAI-800829-B-6/Lecture5/E04_attributes.py
+++ AP4CSAI-800829-B-6/Lecture5/E04_attributes.py
@@ -22,14 +22,14 @@ print(p2.x, p2.y)
 # Class attributes are defined in the class body
 # They are shared by all instances of the class
 
-class Test:
-    x = 0
-    y = 10
+# class Test:
+#     x = 0
+#     y = 10
 
-t1 = Test()
-t2 = Test()
+# t1 = Test()
+# t2 = Test()
 
-t1.x = 5  # creates an instance attribute for t1 only
+# t1.x = 5  # creates an instance attribute for t1 only
 
-print(t1.x)  # 5  (instance attribute)
-print(t2.x)  # 0  (still class attribute)
+# print(t1.x)  # 5  (instance attribute)
+# print(t2.x)  # 0  (still class attribute)
