fd00a9db960d2e9a206b9ab807bdf43d9aa54727
Javad Pourmostafa <j.pourmostafa@tilburguniversity.edu>
2026-04-07 12:53:04 +0200

    update the example with id()

--- AP4CSAI-800829-B-6/Lecture5/E01_variable.py
+++ AP4CSAI-800829-B-6/Lecture5/E01_variable.py
@@ -1,5 +1,7 @@
+# Assign a string to the variable
 a_string_variable = "Hello world!"
-print(type(a_string_variable))
+print(type(a_string_variable), id(a_string_variable))
 
+# Reassign the variable to an integer
 a_string_variable = 42
-print(type(a_string_variable))
+print(type(a_string_variable), id(a_string_variable))
