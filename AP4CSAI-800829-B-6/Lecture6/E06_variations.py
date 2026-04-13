a56863eb96673c3324b41d2ed288f6a80bf46913
Javad Pourmostafa <j.pourmostafa@tilburguniversity.edu>
2026-04-13 16:56:50 +0200

    variation changed

--- AP4CSAI-800829-B-6/Lecture6/E06_variations.py
+++ AP4CSAI-800829-B-6/Lecture6/E06_variations.py
@@ -51,11 +51,11 @@ class Professor(Person):
 # print(ta.study())
 
 # Variation C
-class TA(Student, Professor):
-    def get_role(self):
-        return "I am a teaching assistant!"
+# class TA(Student, Professor):
+#     def get_role(self):
+#         return "I am a teaching assistant!"
 
-ta = TA("Maureen", 123, 1213)
-print(ta.employee_id)
+# ta = TA("Maureen", 123, 1213)
+# print(ta.employee_id)
 
 
